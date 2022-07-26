b2626dd0b14cacdc2e392b1ad96a6f4dc1bf7524
Kollecollier <kristoffer.collier@live.se>
2022-07-26 11:40:18 +0000

    prepare deploy

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestDjango(TestCase):
+
+    def test_this_works(self):
+        self.assertEqual(1, 1)
+
+    def test_this_works2(self):
+        self.assertEqual(1, 1)
+
+    def test_this_works3(self):
+        self.assertEqual(1, 1)
+
+    def test_this_works4(self):
+        self.assertEqual(1, 1)
